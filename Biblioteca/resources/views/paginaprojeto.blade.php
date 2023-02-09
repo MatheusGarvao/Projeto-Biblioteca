710ce2296d6f1416e40065a26a9a5dd1464999da
Matheus Garvão <matheusgarvao@edu.unifil.br>
2023-02-09 15:10:06 -0300

    Permissão para ter mais de um aluno no projeto e comentários adicionados

--- Biblioteca/resources/views/paginaprojeto.blade.php
+++ Biblioteca/resources/views/paginaprojeto.blade.php
@@ -24,9 +24,10 @@
 
             <section>
                 <h3>Desenvolvido por:</h3>
-                <p>
-                    {{ $projeto->nomealuno }}
-                </p>
+                <?php $alunos = explode(',', $projeto->nomealuno); ?>
+                @foreach ($alunos as $aluno)
+                    <p>{{ $aluno }}</p>
+                @endforeach
             </section>
 
             <section>
@@ -37,7 +38,7 @@
             </section>
 
             <div class="botaoprojeto">
-                <h3>Clique no botão abaixo para abrir o site do projeto!</h3>
+                <h3>Clique no botão abaixo para abrir o link do projeto!</h3>
                 <div>
                     <a target="_blank" href='{{ route('projeto.link', ['id' => $projeto->id]) }}'>
                         <div>PROJETO</div>
@@ -49,4 +50,25 @@
     </div>
 
     </div>
+
+    <div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://biblioteca-de-trabalhos.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<script id="dsq-count-scr" src="//biblioteca-de-trabalhos.disqus.com/count.js" async></script>
 @endsection
