481d5940374ef7329755b97eb6a0b1047d73d50e
Matheus Garvão <matheusgarvao@edu.unifil.br>
2023-02-17 17:58:03 -0300

    Update biblioteca
    Faça rodar, se vira Matheus

--- Biblioteca/app/Http/Controllers/PrincipalController.php
+++ Biblioteca/app/Http/Controllers/PrincipalController.php
@@ -14,11 +14,25 @@ class PrincipalController extends Controller
         if ($naoEncontrado == 1) {
             return "Projeto não encontrado! (adicionar notificação / página não encontrado)";
         }
+        $this->incrementar(1);
+        return View('MainPage')->with(["projetos" => $this->getProjetos()]);
+    }
 
-        $pagina = Principal::Find(1);
+    function getRelatorio()
+    {
+        $this->incrementar(2);
+        return View('Relatorio')->with(["projetos" => $this->getProjetos()]);
+    }
+
+    protected function getProjetos()
+    {
+        return Projeto::all()->sortBy('nomeprojeto')->where('projetoativo', 1);
+    }
+
+    protected function incrementar($pag)
+    {
+        $pagina = Principal::Find($pag);
         $pagina->quantidadevisitadas++;
         $pagina->save();
-        $projetos = Projeto::all()->sortBy('nomeprojeto')->where('projetoativo', 1);
-        return View('MainPage')->with(["projetos" => $projetos]);
     }
 }
---
+++ Biblioteca/resources/views/relatorio.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.paginas', ['text' => 'Relatório'])
+
+@section('estilos')
+    <link href="{{ asset('css/Header.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/Padrao.css') }}" rel="stylesheet" type="text/css">
+@endsection
